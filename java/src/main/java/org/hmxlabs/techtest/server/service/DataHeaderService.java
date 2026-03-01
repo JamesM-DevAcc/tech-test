f4e19f0d9c5422df21da164e95273e1690c0c48a
James Mackenzie <>
2026-03-01 18:08:40 +0000

    cleaning up - removing c# and java sections as not needed. Starting Point

--- java/src/main/java/org/hmxlabs/techtest/server/service/DataHeaderService.java
+++
@@ -1,7 +0,0 @@
-package org.hmxlabs.techtest.server.service;
-
-import org.hmxlabs.techtest.server.persistence.model.DataHeaderEntity;
-
-public interface DataHeaderService {
-    void saveHeader(DataHeaderEntity entity);
-}
